fcad3499df4ac083b48baa73be97e43ef63aeeb7
E.A <69919294+arsheed2000@users.noreply.github.com>
2024-07-04 12:44:21 +0200

    Update Main.java
    Final version that allows the user to choose between default dadabase and user created one

--- src/main/java/new_test_allergy_2/Main.java
+++ src/main/java/new_test_allergy_2/Main.java
@@ -1,7 +1,18 @@
 package new_test_allergy_2;
 
+import java.util.Scanner;
+    
 public class Main {
     public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        // Prompt the user to choose the database URL
+        System.out.println("Enter the database URL (or press Enter to use the default database):");
+        String userInput = input.nextLine();
+
+        // Set the database URL in the DatabaseConnection class
+        DatabaseConnection.setUrl(userInput);
+        
         // Step 1: Create the database tables
         System.out.println("Creating tables...");
         CreateTables.main(args);
