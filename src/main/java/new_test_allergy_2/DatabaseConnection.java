ba93a816075ba055295a3bca06f251022dc51e7b
E.A <69919294+arsheed2000@users.noreply.github.com>
2024-07-04 12:29:54 +0200

    Update DatabaseConnection.java
    Updated DatabaseConnection to accept URLs inputed by user

--- src/main/java/new_test_allergy_2/DatabaseConnection.java
+++ src/main/java/new_test_allergy_2/DatabaseConnection.java
@@ -5,10 +5,18 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
-    private static final String URL = "jdbc:sqlite:identifier.sqlite";
+    private static String url;
 
     // Method to establish a connection to the SQLite database
     public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(URL);
+        if (url == null || url.isEmpty()) {
+            throw new SQLException("Database URL is not set.");
+        }
+        return DriverManager.getConnection(url);
+    }
+
+    // Method to set the database URL
+    public static void setUrl(String url) {
+        DatabaseConnection.url = url;
     }
 }
